486cf25fa7aa4b23ebdc26a17ac7d51028057e4e
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-12-02 10:26:25 +0530

    Launcher

---
+++ app/src/main/java/com/example/launcherdemo/HomeScreenFragment.java
@@ -0,0 +1,56 @@
+package com.example.launcherdemo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class HomeScreenFragment extends Fragment {
+
+    ImageView imageViewDrawer;
+    public HomeScreenFragment() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home_screen,container,false);
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imageViewDrawer = view.findViewById(R.id.icon_drawer);
+
+
+
+        imageViewDrawer.setOnClickListener(v -> loadFragment(new AppsDrawerFragment()));
+
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            requireActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+            return true;
+        }
+
+        return false;
+    }
+
+}
